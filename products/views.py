2c9587459e7f288bbc76ac110a1b8c06a9e4dc73
Dmitriy Sidorov <d_sidorov@wargaming.net>
2020-01-02 21:18:54 +0300

    add product create form

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductFrom
 
 
 # Create your views here.
@@ -10,3 +11,16 @@ def product_detail_view(request):
         'description': obj.description
     }
     return render(request, "products/product_detail.html", context)
+
+
+def product_create_view(request):
+    form = ProductFrom(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductFrom()
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'products/product_create.html', context)
